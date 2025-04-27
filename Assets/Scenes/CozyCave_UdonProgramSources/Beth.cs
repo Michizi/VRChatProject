1aab6c7acb432e7a5840cb48e821b4e4ffc795b9
Karlo Rosenthal <krosentha21@student.foi.hr>
2025-04-27 17:22:31 +0300

    Merge success

---
+++ Assets/Scenes/CozyCave_UdonProgramSources/Beth.cs
@@ -0,0 +1,45 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+
+public class Beth : UdonSharpBehaviour
+{
+    public Animator animator;
+    public float interval = 45f;
+    public float yawn_interval = 15f;
+    private float timer;
+    private float yawn_timer;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        timer = interval;
+        yawn_timer = yawn_interval;
+    }
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        yawn_timer -= Time.deltaTime;
+
+        if (timer <= 0f)
+        {
+            timer = interval;
+            animator.SetBool("Bored", true);
+        }
+
+        if (yawn_timer <= 0f && animator.GetBool("Bored"))
+        {
+            yawn_timer = yawn_interval;
+            animator.SetTrigger("Yawn");
+        }
+        else if (yawn_timer <= 0f && !animator.GetBool("Bored"))
+        {
+            yawn_timer = yawn_interval;
+        }
+    }
+}
