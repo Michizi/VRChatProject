2d9a790d07a79a06cb30091894aa2ae1b28c5504
Mico Pelander <mico.pelander@gmail.com>
2025-04-27 08:36:28 +0300

    Added content
    1 Npc, materials, npc animation, npc ai, few meshes, mirror, entrance jingle.

---
+++ Assets/NPCs/Fox/NPCAnimationController.cs
@@ -0,0 +1,28 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class NPCAnimationController : UdonSharpBehaviour
+{
+    public Animator animator;
+    public float movementThreshold = 0.01f;
+
+    private Vector3 lastPosition;
+
+    void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        float movementSpeed = (currentPosition - lastPosition).magnitude / Time.deltaTime;
+
+        bool isWalking = movementSpeed > movementThreshold;
+        animator.SetBool("IsWalking", isWalking);
+
+        lastPosition = currentPosition;
+    }
+}
